f70099f409c0dbd45e0c47fe22c884040ab23097
Grady Ku <gkupdx@gmail.com>
2022-11-04 13:13:22 -0700

    Import and render new route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 //// App.js
+import Navbar from "./components/Navbar.tsx";
+import Landing from "./pages/Landing.tsx";
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Navbar />
+            <Landing />
+          </>
+        } />
+        
+      </Routes>
     </div>
   );
 }
