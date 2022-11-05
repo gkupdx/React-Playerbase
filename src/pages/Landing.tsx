e889abdb295be8a93b66b11da9f5350f274d27a2
Grady Ku <gkupdx@gmail.com>
2022-11-05 13:52:44 -0700

    Move navigation functionality to Button component

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,18 +1,15 @@
 //// Landing.tsx - landing page 
-import { useNavigate } from 'react-router';
 import Button from '../components/Button';
 import '../stylesheets/landing.css';
 
-const Landing = () => {
-    const navigate = useNavigate();
-
+const Landing = () => { 
     return (
         <div className="landing">
             <h1>Welcome to Playerbase</h1>
 
             <div className='btnContainer'>
-                <Button onClick={() => navigate('/login')} name={"Login"}/>
-                <Button onClick={() => navigate('/register')} name={"Register"}/>
+                <Button route={"/login"} name={"Login"}/>
+                <Button route={"/register"} name={"Register"}/>
             </div>
         </div>
     )
