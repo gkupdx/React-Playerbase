6b005bf6586c191d2985baf95d70ae9b112a01ff
Grady Ku <gkupdx@gmail.com>
2022-11-05 13:53:43 -0700

    Import & render new routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 //// App.js
+import { useState } from 'react';
 import Navbar from "./components/Navbar.tsx";
 import Landing from "./pages/Landing.tsx";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from './pages/Profile';
 
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [player, setPlayer] = useState({
+    id: '',
+    name: '',
+    username: '',
+    email: '',
+    joined: ''
+  });
+
+  const loadPlayer = (data) => {
+    setPlayer({
+      id: data.id,
+      name: data.name,
+      username: data.username,
+      email: data.email,
+      joined: data.joined
+    })
+  }
+
   return (
     <div className="App">
       <Routes>
@@ -14,7 +36,24 @@ function App() {
             <Landing />
           </>
         } />
-        
+        <Route path='/login' element={
+          <>
+            <Navbar />
+            <Login loadPlayer={loadPlayer}/>
+          </>
+        } />
+        <Route path='/register' element={
+          <>
+            <Navbar />
+            <Register loadPlayer={loadPlayer}/>
+          </>
+        } />
+        <Route path='/profile/:id' element={
+          <>
+            <Navbar />
+            <Profile player={player}/>
+          </>
+        } />
       </Routes>
     </div>
   );
