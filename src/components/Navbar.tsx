2ad68df102e9db508ad1380859d1d791688a53c3
Grady Ku <gkupdx@gmail.com>
2022-11-05 21:34:14 -0700

    Add Home and Log Out buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,30 @@
 //// Navbar.tsx - "navbar" component
+import { FC } from 'react';
+import { VscHome, VscSignOut } from 'react-icons/vsc';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import '../stylesheets/navbar.css';
 
-const Navbar = () => {
+interface NavProps {
+    loggedIn: boolean
+}
+
+const Navbar: FC<NavProps> = ({ loggedIn }) => {
+    let logOutDisplay: DocumentVisibilityState = 'hidden';
+
+    if (loggedIn) {
+        logOutDisplay = 'visible'
+    }
+
     return (
         <div className="navbar">
-            <a href="https://github.com/gkupdx/React-Playerbase"><FaGithub /> GitHub</a>
-            <a href="https://www.linkedin.com/in/gkupdx/"><FaLinkedin /> Linkedin</a>
+            <div>
+                <a href="/"><VscHome /> Home</a>
+                <a style={{ visibility: logOutDisplay }} href="/signed-out"><VscSignOut /> Log Out</a>
+            </div>
+            <div>
+                <a href="https://github.com/gkupdx/React-Playerbase"><FaGithub /> GitHub</a>
+                <a href="https://www.linkedin.com/in/gkupdx/"><FaLinkedin /> Linkedin</a>
+            </div>
         </div>
     )
 }
