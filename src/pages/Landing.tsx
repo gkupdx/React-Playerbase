b1cb83d97cc66745ac9cb0fdea1c6c2e4a6e9be4
Grady Ku <gkupdx@gmail.com>
2022-11-06 18:59:10 -0800

    Add animations

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,17 +1,19 @@
 //// Landing.tsx - landing page 
+import { motion } from 'framer-motion';
 import Button from '../components/Button';
 import '../stylesheets/landing.css';
 
 const Landing = () => {
     return (
-        <div className="landing">
-            <h1>Welcome to Playerbase</h1>
+        <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0.95 }} transition={{ type: "tween", duration: 0.7 }} className="landing">
 
-            <div className='btnContainer'>
-                <Button route={"/login"} name={"Login"}/>
+            <motion.h1 animate={{ y: 0, opacity: 1 }} initial={{ y: 400, opacity: 0.5 }} transition={{ type: "tween", duration: 0.8 }}>Welcome to Playerbase</motion.h1>
+
+            <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 400, opacity: 0.5 }} transition={{ type: "tween", duration: 1 }} className='btnContainer'>
+                <Button route={"/login"} name={"Log In"}/>
                 <Button route={"/register"} name={"Register"}/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     )
 }
 
