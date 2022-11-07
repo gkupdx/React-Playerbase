e66a3de7eec5557703ee19869f149ef0fa23f4e8
Grady Ku <gkupdx@gmail.com>
2022-11-06 19:00:45 -0800

    Import & render Card component

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,6 @@
 //// Profile.tsx - profile page
 import { FC } from 'react';
+import Card from '../components/Card';
 import '../stylesheets/profile.css';
 
 interface ProfileProps {
@@ -15,7 +16,11 @@ interface ProfileProps {
 const Profile: FC<ProfileProps> = ({ player }) => {
     return (
         <div className='profile'>
-            <h1>Welcome back, {player.username}</h1>
+            <h1>Welcome back, {player.username}!</h1>
+
+            <div className='cardsContainer'>
+                <Card name={"Card #1"}/>
+            </div>
         </div>
     )
 }
