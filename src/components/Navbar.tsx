aa2d9838d907046c9dfa0f98deada0ca5bd3e9f9
Grady Ku <gkupdx@gmail.com>
2022-11-08 21:06:51 -0800

    Update log out functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,35 @@
 //// Navbar.tsx - "navbar" component
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authenticateUser } from '../features/authenticate';
 import { VscSignOut } from 'react-icons/vsc';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import '../stylesheets/navbar.css';
 
 interface NavProps {
-    loggedIn: boolean
+    userID: number,
 }
 
-const Navbar: FC<NavProps> = ({ loggedIn }) => {
+const Navbar: FC<NavProps> = ({ userID }) => {
     const navigate = useNavigate();
-    let logOutDisplay: DocumentVisibilityState = 'hidden';
+    const dispatch = useDispatch();
 
-    if (loggedIn) {
-        logOutDisplay = 'visible'
-    }
+    // on log out, hide "Log Out" link, clear localStorage item
+    const handleLogOut = () => {
+        localStorage.removeItem("LOGGED_IN");
+        dispatch(authenticateUser({ authenticated: 0 }));
+    } 
 
     return (
         <div className="navbar">
             <div>
                 <img onClick={() => navigate('/')} src={require('../assets/playerbase_logo2.png')} alt="Playerbase" />
-                <a style={{ visibility: logOutDisplay }} href="/logged-out"><VscSignOut /> Log Out</a>
+                <a onClick={handleLogOut} className={userID !== 0 ? 'logOutVisible' : 'logOutHidden'} href="/logged-out"><VscSignOut /> log out</a>
             </div>
             <div>
-                <a href="https://github.com/gkupdx/React-Playerbase"><FaGithub /> GitHub</a>
-                <a href="https://www.linkedin.com/in/gkupdx/"><FaLinkedin /> Linkedin</a>
+                <a href="https://github.com/gkupdx/React-Playerbase"><FaGithub /> github</a>
+                <a href="https://www.linkedin.com/in/gkupdx/"><FaLinkedin /> linkedin</a>
             </div>
         </div>
     )
