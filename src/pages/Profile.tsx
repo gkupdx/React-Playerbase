06e32ee973511ebcd7ad04808b7f6487998de537
Grady Ku <gkupdx@gmail.com>
2022-11-08 21:07:14 -0800

    Add state persistence of user profile data on page refreshes

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,7 @@
 //// Profile.tsx - profile page
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { FaTools } from 'react-icons/fa';
+// import Youtube, { YouTubeProps } from 'react-youtube';
 import Card from '../components/Card';
 import '../stylesheets/profile.css';
 
@@ -10,16 +12,32 @@ interface ProfileProps {
         username: string,
         email: string,
         joined: string
-    }
+    },
+    setPlayer: (data: object) => void,
 }
 
-const Profile: FC<ProfileProps> = ({ player }) => {
+const Profile: FC<ProfileProps> = ({ player, setPlayer }) => {
+    const iconStyle = {
+        fontSize: '5rem',
+        color: '#ffcc4d',
+        opacity: '0.9',
+    }
+
+    // check to see if a user is logged in on page refresh
+    useEffect(() => {
+        const isLoggedIn = localStorage.getItem("LOGGED_IN");
+        if (isLoggedIn) {
+            const playerInfo = JSON.parse(isLoggedIn);
+            setPlayer(playerInfo);
+        }
+    }, [setPlayer]);
+
     return (
         <div className='profile'>
             <h1>Welcome back, {player.username}!</h1>
 
             <div className='cardsContainer'>
-                <Card name={"Card #1"}/>
+                <Card name={"Build"} icon={<FaTools style={iconStyle} />} />
             </div>
         </div>
     )
