656bdb9adf4da5445d41e4cebec78d0fba2d89d2
Grady Ku <gkupdx@gmail.com>
2022-11-12 16:46:52 -0800

    Add new Error.tsx page component

---
+++ src/pages/Error.tsx
@@ -0,0 +1,22 @@
+//// Error.tsx - the "404" error page for when a user tries to brute-force a URL endpoint
+import { useNavigate } from 'react-router-dom';
+import '../stylesheets/error.css';
+
+const Error = () => {
+    const navigate = useNavigate(); 
+
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>
+                The page you are looking for might have been removed,
+                had its name changed, or is temporarily unavailable.
+            </p>
+
+            <button onClick={() => navigate('/')} className='primaryBtn'>Return Home</button>
+        </div>
+    )
+}
+
+export default Error;
