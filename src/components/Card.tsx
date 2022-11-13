3650a9ca28eacd35d813aa838be8b03cf1c0273c
Grady Ku <gkupdx@gmail.com>
2022-11-12 16:46:24 -0800

    Replace className with style object

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,8 +7,23 @@ interface CardProps {
 }
 
 const Card: FC<CardProps> = ({ name, icon }) => {
+    const cardStyle: React.CSSProperties = {
+        width: '225px',
+        height: '325px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        color: 'beige',
+        fontFamily: 'Courier Prime, monospace',
+        fontSize: '1.7rem',
+        borderRadius: '10px',
+        boxShadow: '0 2px 5px 2px #000',
+        cursor: 'pointer'
+    }
+
     return (
-        <div className="card">
+        <div style={cardStyle}>
             {name}
             {icon}
         </div>
