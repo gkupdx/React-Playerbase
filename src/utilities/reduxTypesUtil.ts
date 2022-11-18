276e263bb338dbdd429d894deb0f6452a7cd5d71
Grady Ku <gkupdx@gmail.com>
2022-11-17 18:57:45 -0800

    Rename file to reduxTypesUtil.ts

--- src/utilities/reduxTypesUtil.ts
+++ src/utilities/reduxTypesUtil.ts
@@ -1,4 +1,5 @@
 //// rootState.tsx - utility file for defining types for Redux + TypeScript
+import { useDispatch } from 'react-redux';
 import { store } from '../index';
 
 // useSelector(state: RootState)
@@ -6,3 +7,6 @@ export type RootState = ReturnType<typeof store.getState>
 
 // useDispatch() - for thunks & other middleware
 export type AppDispatch = typeof store.dispatch;
+
+// useDispatch() - dispatch function
+export const useAppDispatch = () => useDispatch<AppDispatch>();
