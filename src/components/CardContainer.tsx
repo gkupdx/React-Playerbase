08df6b06b376ec7d24c2035c71e6b4ca4992c251
Grady Ku <gkupdx@gmail.com>
2022-11-18 14:20:01 -0800

    Add props, update content

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,9 +1,17 @@
 //// CardContainer.tsx - card container component
+import { FC } from 'react';
 import { motion } from 'framer-motion';
-import { FaDiscord, FaReddit, FaYoutube } from 'react-icons/fa';
+import { BsNewspaper } from 'react-icons/bs';
+import { GiCardBurn } from 'react-icons/gi';
+import { FaYoutube } from 'react-icons/fa';
 import Card from "./Card";
 
-const CardContainer = () => {
+interface CContainerProps {
+    id: number,
+}
+
+const CardContainer: FC<CContainerProps> = ({ id }) => {
+
     const containerStyle: React.CSSProperties = {
         width: "1200px",
         height: "400px",
@@ -22,10 +30,10 @@ const CardContainer = () => {
     }
 
     return (
-        <motion.div animate={{ x: 0, opacity: 1 }} initial={{ x: 300, opacity: 0.7 }} transition={{ type: "tween", duration: 0.5 }} style={containerStyle}>
-            <Card name={"Reddit"} icon={<FaReddit style={iconStyle} />} />
-            <Card name={"Discord"} icon={<FaDiscord style={iconStyle} />} />
-            <Card name={"YouTube"} icon={<FaYoutube style={iconStyle} />} />
+        <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0.7 }} transition={{ type: "tween", duration: 0.8 }} style={containerStyle}>
+            <Card route={`/profile/${id}/decks`} name={"Your Decks"} icon={<GiCardBurn style={iconStyle} />} />
+            <Card route={'/metagame'} name={"Metagame"} icon={<BsNewspaper style={iconStyle} />} />
+            <Card route={'/saved-videos'} name={"Your Videos"} icon={<FaYoutube style={iconStyle} />} />
         </motion.div>
     )
 }
