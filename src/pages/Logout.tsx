05c477ca5d3cea0a8ec01c5f08e4c0c06acef5c4
Grady Ku <gkupdx@gmail.com>
2022-11-17 21:58:54 -0800

    Add animations

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,17 +1,18 @@
 //// Logout.tsx - log out page component
+import { motion } from 'framer-motion';
 import Button from "../components/Button";
 import "../stylesheets/logout.css";
 
 const Logout = () => {
     return (
-        <div className="logout">
+        <motion.div animate={{ x: 0, opacity: 1 }} initial={{ x: 300, opacity: 0.7 }} transition={{ type: "tween", duration: 0.5 }} className="logout">
             <h1>You Have Successfully<br />Logged Out!</h1>
 
             <div>
                 <p>Not what you were looking for?</p>
                 <Button route={"/login"} name={"Log In"} />
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,17 +1,18 @@
 //// Success.tsx - update success page for profile updates
+import { motion } from 'framer-motion';
 import Button from "../components/Button";
 import '../stylesheets/success.css';
 
 const Success = () => {
     return (
-        <div className="success">
+        <motion.div animate={{ x: 0, opacity: 1 }} initial={{ x: 300, opacity: 0.7 }} transition={{ type: "tween", duration: 0.5 }} className="success">
             <h1>Password Update Successful!</h1>
 
             <div>
                 <p>For your safety, please log back in with your updated password.</p>
                 <Button route={"/login"} name={"Log In"} />
             </div>
-        </div>
+        </motion.div>
     )
 }
 
