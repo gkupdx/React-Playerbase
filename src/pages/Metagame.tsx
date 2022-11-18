84137c7170950b6a7a952cac3e37195562dd15de
Grady Ku <gkupdx@gmail.com>
2022-11-18 14:21:16 -0800

    Add new Metagame.tsx page component

---
+++ src/pages/Metagame.tsx
@@ -0,0 +1,12 @@
+//// Metagame.tsx - page component for metagame news for Standard format (use mtggoldfish.com)
+
+const Metagame = () => {
+    return (
+        <div className="metagame">
+            <h1>Trending decks in Standard format:</h1>
+            <p>[link to mtggoldfish.com here]</p>
+        </div>
+    )
+}
+
+export default Metagame;
