384037114a8cfea9d468be7ab4acfe5db8142fc3
Grady Ku <gkupdx@gmail.com>
2022-11-18 14:19:29 -0800

    Update props

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,6 @@
 //// Landing.tsx - landing page 
 import { motion } from 'framer-motion';
-import { FC, useEffect } from 'react';
-import { checkLoginStatus } from '../utilities/checkLoginUtility';
+import { FC } from 'react';
 import Button from '../components/Button';
 import '../stylesheets/landing.css';
 
@@ -13,14 +12,12 @@ interface LandingProps {
         email: string,
         joined: string,
     },
-    setPlayer: (data: object) => void,
+    auth: {
+        authenticated: number,
+    }
 }
 
-const Landing: FC<LandingProps> = ({ player, setPlayer }) => {
-    useEffect(() => {
-        checkLoginStatus(setPlayer);
-    }, [setPlayer])
-
+const Landing: FC<LandingProps> = ({ player, auth }) => {
     return (
         <div className="landing">
             <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 400, opacity: 0.7 }} transition={{ type: "tween", duration: 0.7 }} className='headerContainer'>
@@ -28,11 +25,12 @@ const Landing: FC<LandingProps> = ({ player, setPlayer }) => {
                 <h2>your one-stop hub.</h2>
             </motion.div>
 
-            {player.username === '' ?
+            {auth.authenticated === 0 ?
                 <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 400, opacity: 0.7 }} transition={{ type: "tween", duration: 0.8 }} className='btnContainer'>
                     <Button route={"/login"} name={"Log In"} />
                     <Button route={"/register"} name={"Register"} />
-                </motion.div> :
+                </motion.div> 
+                :
                 <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 400, opacity: 0.7 }} transition={{ type: "tween", duration: 0.8 }} style={{ flexDirection: 'column'}} className='btnContainer'>
                     <h3>Hey <span style={{ color: '#EF5B5B' }}>{player.username}</span>, got new decks to brew?</h3>
                     <Button route={`/profile/${player.id}`} name={"Back To Brewing!"} />
