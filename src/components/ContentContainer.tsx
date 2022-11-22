ea9bf13f150e4098fdebdc658b36a84194b685bb
Grady Ku <gkupdx@gmail.com>
2022-11-21 20:43:33 -0800

    Update content

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -1,15 +1,12 @@
 //// CardContainer.tsx - card container component
-import { FC } from 'react';
 import { motion } from 'framer-motion';
 import { GiCardBurn } from 'react-icons/gi';
 import { FaYoutube } from 'react-icons/fa';
-import Section from "./Section";
 
-interface CContainerProps {
-    id: number,
-}
+import ScrollWrapper from './ScrollWrapper';
+import Metagame from './Metagame';
 
-const ContentContainer: FC<CContainerProps> = ({ id }) => {
+const ContentContainer = () => {
 
     const containerStyle: React.CSSProperties = {
         width: "1800px",
@@ -30,8 +27,20 @@ const ContentContainer: FC<CContainerProps> = ({ id }) => {
 
     return (
         <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0.7 }} transition={{ type: "tween", duration: 0.8 }} style={containerStyle}>
-            <Section name={"Metagame"} icon={<GiCardBurn style={iconStyle} />} />
-            <Section name={"Your Videos"} icon={<FaYoutube style={iconStyle} />} />
+            <ScrollWrapper>
+                <div>
+                    <h2>Metagame</h2>
+                    <GiCardBurn style={iconStyle} />
+                </div>
+
+                <Metagame />
+            </ScrollWrapper>
+
+            <ScrollWrapper>
+                <h2>Your Videos</h2>
+                <FaYoutube style={iconStyle} />
+
+            </ScrollWrapper>
         </motion.div>
     )
 }
