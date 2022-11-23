4cf5c73fa53be8c0751894f1f29d056fc5431b7c
Grady Ku <gkupdx@gmail.com>
2022-11-22 19:37:42 -0800

    Replace style object with className

--- src/components/ScrollWrapper.tsx
+++ src/components/ScrollWrapper.tsx
@@ -1,20 +1,8 @@
 //// ScrollWrapper.tsx - wrapper component to provide child components a Y-scrollable container
 
 const ScrollWrapper = (props: React.PropsWithChildren) => {
-    const wrapperStyle: React.CSSProperties = {
-        overflowY: 'scroll',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        width: '800px',
-        height: '600px',
-        border: '1px solid dimgrey',
-        padding: '15px',
-    }
-
     return (
-        <div style={wrapperStyle}>
+        <div className='scrollWrapper'>
             {props.children}
         </div>
     )
