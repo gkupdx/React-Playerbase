8e24f3563fc85c4c4c2cfd9f35b62f720a505443
Grady Ku <gkupdx@gmail.com>
2022-12-09 18:47:14 -0800

    Remove preloading of deck list; allow adding of individual cards

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,33 +1,16 @@
 //// Deck.tsx - individual component for a deck
-import { FC, useState, useEffect }  from 'react';
-import { checkCards } from '../utilities/checkDeckInfoUtil';
+import React, { FC }  from 'react';
 import CardHeader from '../components/CardHeader';
 
 interface DeckProps {
-    deckName: string,
-    cardAdd: boolean,
-    cardName: string,
+    cardList: React.ComponentState,
 }
 
-const Deck: FC<DeckProps> = ({ deckName, cardAdd, cardName }) => {
-    const [deckList, setDeckList] = useState(['']);
-
-    useEffect(() => {
-        let cardImgList: string[] | undefined = [];
-    
-        cardImgList = checkCards(deckName, cardAdd, cardName);
-        if (cardImgList) {
-            setDeckList(cardImgList);
-            // console.log(deckList);
-        }
-    }, [deckName, cardAdd, cardName, setDeckList]) ;
-
+const Deck: FC<DeckProps> = ({ cardList }) => {
     return (
         <div className='deck'>
-            {/* <p>decklist goes here</p> */}
-            {deckList.map((cardImg) => {
-                let index = deckList.indexOf(cardImg);
-                return <CardHeader key={index} cardHeader={cardImg} />
+            {cardList.map((item: React.ComponentState) => {
+                return <CardHeader key={item.id} imageSrc={item.cardImageUri} />
             })}
         </div>
     )
