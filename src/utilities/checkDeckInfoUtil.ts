9acf8afe59401d7c1e05885ae5c49b58a4860d02
Grady Ku <gkupdx@gmail.com>
2022-12-09 18:49:10 -0800

    Comment out unused code (temporary measure)

--- src/utilities/checkDeckInfoUtil.ts
+++ src/utilities/checkDeckInfoUtil.ts
@@ -1,8 +1,9 @@
 //// checkDeckInfoUtil.ts - utility functions for getting the information about a deck
-import * as Scry from 'scryfall-sdk';
-import { grixisMidrange } from "../api/decklists/grixis_midrange";
+// import React from 'react';
+// import * as Scry from 'scryfall-sdk';
+// import { grixisMidrange } from "../api/decklists/grixis_midrange";
 
-type CardImage = string | undefined;
+// type ImageURI = string | undefined; 
 
 export const checkDeckColor = (deckName: string) => {
     let colors: string = '';
@@ -20,20 +21,47 @@ export const checkDeckColor = (deckName: string) => {
     return colors;
 }
 
-export const checkCards = (deckName: string) => {
-    let imageSrcArray: string[] = [''];
+// export const checkCards = (deckName: string, cardAdd: boolean, cardName: string) => {
+//     let imageSrcArray: string[] = [];
+//     // separate request arrays of 10 strings per array (as per Scryfall API docs)
+//     let reqOne: string[] = [];
+//     let reqTwo: string[] = [];
+//     let reqThree: string[] = [];
+//     let reqFour: string[] = [];
+//     let reqFive: string[] = [];
+//     let reqSix: string[] = [];
 
-    if (deckName.includes("Grixis")) {
-        grixisMidrange.forEach(async (cardName) => {
-            const card = await Scry.Cards.byName(cardName);
-            const imageURI: CardImage = (card.image_uris?.normal);
+//     const fetchGrixis = (reqNumber: string[]) => {
+//         reqNumber.forEach(async (cardName) => {
+//             const card = await Scry.Cards.byName(cardName);
+//             const imageURI: ImageURI = (card.image_uris?.small);
 
-            if (imageURI) {
-                const imageSrc = imageURI.toString();
-                imageSrcArray.push(imageSrc);
-            }
-        })
-    }
+//             if (imageURI) {
+//                 const imageSrc = imageURI.toString();
+//                 imageSrcArray.push(imageSrc);
+//             }
+//         });
+//     }
 
-    return imageSrcArray;
-}
+//     if (deckName.includes("Grixis")) {
+//         for (let i = 0; i < grixisMidrange.length; ++i) {
+//             if (i <= 9) {
+//                 reqOne.push(grixisMidrange[i])
+//             } else if (i > 9 && i <= 19) {
+//                 reqTwo.push(grixisMidrange[i])
+//             } else if (i > 19 && i <= 29) {
+//                 reqThree.push(grixisMidrange[i])
+//             } else if (i > 29 && i <= 39) {
+//                 reqFour.push(grixisMidrange[i])
+//             } else if (i > 39 && i <= 49) {
+//                 reqFive.push(grixisMidrange[i])
+//             } else {
+//                 reqSix.push(grixisMidrange[i])
+//             }
+//         }
+
+//         setTimeout(fetchGrixis, 1000);
+//     }
+
+//     return imageSrcArray;
+// }
