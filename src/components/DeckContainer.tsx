38012f05a0d11e264710b6c60e8ab668c80bf2df
Grady Ku <gkupdx@gmail.com>
2022-12-11 20:57:14 -0800

    Remove h2 and icon elements

--- src/components/DeckContainer.tsx
+++ src/components/DeckContainer.tsx
@@ -1,7 +1,7 @@
 //// CardContainer.tsx - card container component
 import { motion } from 'framer-motion';
 import { FC } from 'react';
-import { GiCardBurn } from 'react-icons/gi';
+// import { GiCardBurn } from 'react-icons/gi';
 
 import Deck from './Deck';
 import DeckColor from './DeckColor';
@@ -14,18 +14,15 @@ interface DeckContainerProps {
 }
 
 const DeckContainer: FC<DeckContainerProps> = ({ deckName, cardList }) => {
-    const iconStyle: React.CSSProperties = {
-        fontSize: '5rem',
-        color: '#ffcc4d',
-        opacity: '0.9',
-    }
+    // const iconStyle: React.CSSProperties = {
+    //     fontSize: '5rem',
+    //     color: '#ffcc4d',
+    //     opacity: '0.9',
+    // }
 
     return (
-        <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0.7 }} transition={{ type: "tween", duration: 0.8 }} className='contentContainer'>
+        <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0.7 }} transition={{ type: "tween", duration: 0.8 }} className='deckContainer'>
             <div>
-                <h2>Your Active Deck</h2>
-                <GiCardBurn style={iconStyle} />
-
                 <ScrollWrapper>
                     <div className='deckHeader' style={{ backgroundImage: `url(${nicolBolas})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
                         <h2>Grixis Midrange</h2>
