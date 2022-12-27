cde36a55569db0f0eccdec6492c2b85e0c36ea63
Grady Ku <gkupdx@gmail.com>
2022-12-26 18:19:11 -0800

    Replace CardHeader component with <img> element

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,17 +1,29 @@
 //// Deck.tsx - individual component for a deck
-import React, { FC }  from 'react';
-import CardHeader from '../components/CardHeader';
+import React, { FC } from 'react';
 
 interface DeckProps {
     cardList: React.ComponentState,
 }
 
 const Deck: FC<DeckProps> = ({ cardList }) => {
+    const cardImgStyle = {
+        width: '150px',
+        height: '200px',
+        boxShadow: '0 5px 5px 0 #000',
+    }
+
+    const handleContextMenu = (event: React.MouseEvent<HTMLImageElement>) => {
+        event.preventDefault();
+
+        console.log("RMB clicked");
+    }
+
     return (
         <div className='deck'>
             {cardList[0].id === 0 && <p>Cards added to the deck will be displayed here</p>}
-            {cardList[0].id !== 0 && cardList.map((item: React.ComponentState) => {
-                return <CardHeader key={item.id} imageSrc={item.cardImageUri}/>
+
+            {cardList[0].id !== 0 && cardList.map((card: React.ComponentState) => {
+                return <img key={card.id} onContextMenu={(e) => handleContextMenu(e)} style={cardImgStyle} src={card.cardImageUri} alt={card.cardImageUri} />
             })}
         </div>
     )
