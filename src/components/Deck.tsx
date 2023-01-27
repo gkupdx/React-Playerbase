62226fe8c242cca7c8865c64949911e2a9ec3093
Grady Ku <gkupdx@gmail.com>
2023-01-26 23:04:43 -0800

    Replace img element with div element; add card counter element

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -6,24 +6,16 @@ interface DeckProps {
 }
 
 const Deck: FC<DeckProps> = ({ cardList }) => {
-    const cardImgStyle = {
-        width: '150px',
-        height: '200px',
-        boxShadow: '0 5px 5px 0 #000',
-    }
-
-    const handleContextMenu = (event: React.MouseEvent<HTMLImageElement>) => {
-        event.preventDefault();
-
-        console.log("RMB clicked");
-    }
 
     return (
         <div className='deck'>
             {cardList[0].id === 0 && <p>Cards added to the deck will be displayed here</p>}
 
             {cardList[0].id !== 0 && cardList.map((card: React.ComponentState) => {
-                return <img key={card.id} onContextMenu={(e) => handleContextMenu(e)} style={cardImgStyle} src={card.cardImageUri} alt={card.cardImageUri} />
+                return (
+                    <div key={card.id} style={{ width: '170px', height: '235px', backgroundImage: `url(${card.cardImageUri})`, backgroundPosition: 'center', backgroundSize: 'cover', boxShadow: '0 5px 5px 0 #000'}}>
+                        <p className='cardCounter'>x{card.count}</p>
+                    </div>)
             })}
         </div>
     )
